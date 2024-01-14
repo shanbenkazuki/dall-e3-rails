24441e62f3a8509aa3eb454edc0d3825cf8c437c
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-01-14 15:17:51 +0900

    受け取ったエラーを表示する

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,9 +1,20 @@
 class ImagesController < ApplicationController
   def index
     @image_url = session[:image_url]
+    @error = session[:error]
   end
 
   def create
+    prompt = params[:prompt]
+    response = OpenAiService.generate_image(prompt)
+
+    if response.key?("error")
+      session[:error] = response["error"]
+    else
+      session[:image_url] = response["data"][0]["url"]
+      session[:error] = nil
+    end
+
     redirect_to images_path
   end
 end
